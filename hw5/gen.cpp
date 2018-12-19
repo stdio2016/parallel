4e147e3e4fc16e63e69e28fb2ef3368d8cbfdabc
stdio2016 <randomfrank8@gmail.com>
2018-12-19 23:07:30 +0800

    fix gen format

--- hw5/gen.cpp
+++ hw5/gen.cpp
@@ -4,9 +4,10 @@ int main(int argc, char *argv[]) {
 	if (argc<2) return 1;
 	std::ofstream f("input");
 	int n = atoi(argv[1]);
-	f << n<< '\n';
+	n = n/3 * 3;
+	f << n << '\n';
 	unsigned seed = argc>2 ? atoi(argv[2]) : 0;
-	for (int i=0;i<n;i++) {
+	for (int i=0;i<n/3;i++) {
 		int r=rand_r(&seed)&255;
 		int g=rand_r(&seed)&255;
 		int b=rand_r(&seed)&255;
